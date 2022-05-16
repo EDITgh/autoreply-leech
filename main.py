092c8c7337c062bb855b2307f33d7fd2bdc09129
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-05-16 21:17:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from pyrogram import Client
+import config
+import os
 
+Autoreply=Client(
+    "Pyrogram Bot",
+    bot_token=os.environ["BOT_TOKEN"],
+    api_id=int(os.environ["API_ID"]),
+    api_hash=os.environ["API_HASH"],
+    plugins=dict(root="MOVIE_TIME")
+)
+
+Autoreply.run()
