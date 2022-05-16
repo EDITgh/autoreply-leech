3f1d87e6a1349c2263afd2f3b0c7a5edd6b881cf
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-05-16 21:25:52 +0530

    Update filters.py

--- AUTO_REPLY/filters.py
+++ AUTO_REPLY/filters.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
 
+
+
+@Client.on_message(filters.regex("h") | filters.regex("H"))
+async def regex(bot, message): 
+    await message.reply_text(
+        text="/leech")
