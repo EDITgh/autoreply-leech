5da615d1066d61dba03ebf7e3673a570a2a6c486
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-05-16 21:22:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Autoreply=Client(
     bot_token=os.environ["BOT_TOKEN"],
     api_id=int(os.environ["API_ID"]),
     api_hash=os.environ["API_HASH"],
-    plugins=dict(root="MOVIE_TIME")
+    plugins=dict(root="AUTO_REPLY")
 )
 
 Autoreply.run()
